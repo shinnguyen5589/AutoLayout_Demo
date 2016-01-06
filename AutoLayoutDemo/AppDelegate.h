04acbc5bd7e2a3ebcbc549c6bddedc8d06c87697
jv-it-hoangdung <hoangdung@jv-it.com.vn>
2016-01-06 12:43:29 +0700

    - push all project

---
+++ AutoLayoutDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AutoLayoutDemo
+//
+//  Created by Zdc006-Hoang Dung on 1/6/16.
+//  Copyright © 2016 Test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AutoLayoutDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutoLayoutDemo
+//
+//  Created by Zdc006-Hoang Dung on 1/6/16.
+//  Copyright © 2016 Test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
